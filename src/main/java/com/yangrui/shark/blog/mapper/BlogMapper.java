0220f877256796e20f51e89788c5fbefbd54f3a0
杨锐(yangrui-lc) <yangrui-lc@inspur.com>
2020-04-24 17:04:35 +0800

    删除多余方法。

--- src/main/java/com/yangrui/shark/blog/mapper/BlogMapper.java
+++ src/main/java/com/yangrui/shark/blog/mapper/BlogMapper.java
@@ -2,7 +2,6 @@
 package com.yangrui.shark.blog.mapper;
 
 import com.yangrui.shark.blog.data.BlogVO;
-import com.yangrui.shark.data.User;
 import java.util.List;
 import java.util.Map;
 import org.apache.ibatis.annotations.Mapper;
@@ -20,8 +19,6 @@ public interface BlogMapper {
   List<Map<String, String>> getBlogList();
 
   BlogVO getBlogDetail(String blogId);
-
-  User findByUsername(String username);
-
+  
   void deleteBlogById(String id);
 }
